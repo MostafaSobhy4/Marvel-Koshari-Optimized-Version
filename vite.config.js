58106d100bd995c7f91df2da4512051d58257397
MostafaSobhy4 <smmm95607@gmail.com>
2026-04-16 15:02:56 +0200

    vite

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        about: resolve(__dirname, "aboutus.html"),
+        branches: resolve(__dirname, "branches.html"),
+        cart: resolve(__dirname, "cart.html"),
+        contact: resolve(__dirname, "contactus.html"),
+        login: resolve(__dirname, "login.html"),
+        signup: resolve(__dirname, "signup.html"),
+      },
+    },
+  },
+});
