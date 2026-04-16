74e95a19e5a534527018ab0d3ff69717bbdc3d4b
MostafaSobhy4 <smmm95607@gmail.com>
2026-04-16 17:01:51 +0200

    c

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
 import { defineConfig } from "vite";
 import { resolve } from "path";
-import viteImagemin from "vite-plugin-imagemin";
 
 export default defineConfig({
-  plugins: [
-    viteImagemin({
-      webp: {
-        quality: 75,
-      },
-    }),
-  ],
   build: {
     rollupOptions: {
       input: {
